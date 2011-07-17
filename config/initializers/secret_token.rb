6f876e6c9f94ced5f5f4dd24943de23f97384a99
Junya Ogura <junyaogura@gmail.com>
2011-07-17 17:38:07 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuJa::Application.config.secret_token = 'ca2c53cc21106b9bcbf7aa74499437cd7334582e44f037af735e266ad09ac515ca631b768a20c33a70e1a29d5cfa7ae00bd7a8637652770f74acd9cb765844b5'
