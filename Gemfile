31f9169a5df14534c5f4e00609114caeadbe2f82
Junya Ogura <junyaogura@gmail.com>
2013-12-04 16:39:04 +0900

    Update rails to 4.0.2

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.0.0'
 
-gem 'rails', '3.2.15'
+gem 'rails', '4.0.2'
 gem 'pg', '0.17.0'
 gem 'jquery-rails', '3.0.4'
 gem 'newrelic_rpm', '3.6.9.171'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 HerokuJa::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.eager_load = true
+
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
